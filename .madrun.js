4aa43ebb764651d58a1239af7724f91d9f570572
RastaAlex <Rastaalex86@gmail.com>
2021-07-26 19:52:55 +0300

    adding/fix

--- .madrun.js
+++ .madrun.js
@@ -3,7 +3,7 @@
 const {run} = require('madrun');
 
 module.exports = {
-    'lint': () => 'putout lib/bigNumbers.js',
+    'lint': () => 'putout lib/deep-copy.js',
     'fix:lint': () => run('lint', '--fix'),
     'report': () => 'nyc report --reporter=text-lcov | coveralls',
     'coverage': () => 'nyc npm test',
